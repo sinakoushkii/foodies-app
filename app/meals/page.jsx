8ff9a8021667e9e5febade45269e911fc55a30cd
sina koushki <sinaderik7@gmail.com>
2025-03-06 21:16:00 +0330

    database created and meals are rendered in the site

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,23 +1,32 @@
 import Link from "next/link";
+import classes from "./page.module.css"
 import React from "react";
+import MealsGrid from "@/components/Meals/MealsGrid";
+import { getMeals } from "@/lib/meals";
 
-const MealsPage = () => {
+const MealsPage = async() => {
+  const meals=await getMeals();
+
   return (
-    <div>
-      MealsPage
-      <p>
-        <Link href="/">Home</Link>
-      </p>
-      <p>
-        <Link href="/community">community</Link>
-      </p>
-      <p>
-        <Link href="/meals">meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">meals - share</Link>
-      </p>
-    </div>
+   <>
+    <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals}/>
+      </main>
+   </>
   );
 };
 

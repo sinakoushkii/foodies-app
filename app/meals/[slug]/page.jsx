d6a18a1cf479466b055086ff08d567b3c8ca8a58
sina koushki <sinaderik7@gmail.com>
2025-03-07 17:43:23 +0330

    share meal form is implemented

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -3,9 +3,15 @@ import React from "react";
 import classes from "./page.module.css";
 import Image from "next/image";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 const MealsDetail = ({ params }) => {
   const meal = getMeal(params.slug);
+  
+  if(!meal){
+    notFound();
+  }
+
   meal.instructions = meal.instructions.replace(/\n/g, '<br />');
 
   return (
